8992189cf76e53acc902569d830dc259c02681ef
Victor Poglioni <victorrenop16@gmail.com>
2023-05-06 21:11:37 -0300

    Some lint fixes and better comments

--- internal/controllers/ping_controller.go
+++ internal/controllers/ping_controller.go
@@ -6,8 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// HealthChecker is a simple controller for pinging
 type HealthChecker struct{}
 
+// PingHandler is the function responsible for health checking the app
 func (h HealthChecker) PingHandler(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
